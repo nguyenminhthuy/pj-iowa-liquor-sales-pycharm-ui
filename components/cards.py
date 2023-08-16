f793a8b58ca430379239fd51b87b96fb083fdfe3
Nguyen Minh Thuy <ntmthuy.1264140@gmail.com>
2023-08-16 19:41:15 +0700

    Update Callback function for Card component

--- components/cards.py
+++ components/cards.py
@@ -69,420 +69,202 @@ statebottleretail = [
 
 # -------------------------------------------------
 @callback(
-    Output('card_sale_title', 'children'),
+    Output('card_sale_title', 'children'), # sale
     Output('card_total_sales', 'children'),
-    Input('store_y', 'data'),
-)
-def selected_y(y_selected):
-    global title, total_sales
-
-    if y_selected is None:
-        title = f"Year: N/A"
-        total_sales = "$0"
-    else:
-        # 2012
-        if y_selected == 1:
-            title = f"Year: 2012"
-            total_sales = "$100,000,000"
-
-        # 2013
-        if y_selected == 2:
-            title = f"Year: 2013"
-            total_sales = "$100,000,000"
-
-        # 2014
-        if y_selected == 3:
-            title = f"Year: 2014"
-            total_sales = "$100,000,000"
-
-        # 2015
-        if y_selected == 4:
-            title = f"Year: 2015"
-            total_sales = "$100,000,000"
-
-        # 2016
-        if y_selected == 5:
-            title = f"Year: 2016"
-            total_sales = "$100,000,000"
-
-        # 2017
-        if y_selected == 6:
-            title = f"Year: 2017"
-            total_sales = "$100,000,000"
-
-        # 2018
-        if y_selected == 7:
-            title = f"Year: 2018"
-            total_sales = "$100,000,000"
-
-        # 2019
-        if y_selected == 8:
-            title = f"Year: 2019"
-            total_sales = "$100,000,000"
-
-        # 2020
-        if y_selected == 9:
-            title = f"Year: 2020"
-            total_sales = "$100,000,000"
-
-        # 2021
-        if y_selected == 10:
-            title = f"Year: 2021"
-            total_sales = "$100,000,000"
-
-        # 2022
-        if y_selected == 11:
-            title = f"Year: 2022"
-            total_sales = "$100,000,000"
-
-    return title, total_sales
-
-# -------------------------------------------------
-@callback(
-    Output('card_inv_title', 'children'),
+    Output('card_inv_title', 'children'), # inv
     Output('card_total_inv', 'children'),
-    Input('store_y', 'data'),
-)
-def selected_y(y_selected):
-    global title, total_sales
-
-    if y_selected is None:
-        title = f"Year: N/A"
-        total_sales = "$0"
-    else:
-        # 2012
-        if y_selected == 1:
-            title = f"Year: 2012"
-            total_sales = "$100,000,000"
-
-        # 2013
-        if y_selected == 2:
-            title = f"Year: 2013"
-            total_sales = "$100,000,000"
-
-        # 2014
-        if y_selected == 3:
-            title = f"Year: 2014"
-            total_sales = "$100,000,000"
-
-        # 2015
-        if y_selected == 4:
-            title = f"Year: 2015"
-            total_sales = "$100,000,000"
-
-        # 2016
-        if y_selected == 5:
-            title = f"Year: 2016"
-            total_sales = "$100,000,000"
-
-        # 2017
-        if y_selected == 6:
-            title = f"Year: 2017"
-            total_sales = "$100,000,000"
-
-        # 2018
-        if y_selected == 7:
-            title = f"Year: 2018"
-            total_sales = "$100,000,000"
-
-        # 2019
-        if y_selected == 8:
-            title = f"Year: 2019"
-            total_sales = "$100,000,000"
-
-        # 2020
-        if y_selected == 9:
-            title = f"Year: 2020"
-            total_sales = "$100,000,000"
-
-        # 2021
-        if y_selected == 10:
-            title = f"Year: 2021"
-            total_sales = "$100,000,000"
-
-        # 2022
-        if y_selected == 11:
-            title = f"Year: 2022"
-            total_sales = "$100,000,000"
-
-    return title, total_sales
-
-# -------------------------------------------------
-@callback(
-    Output('card_bottle_title', 'children'),
+    Output('card_bottle_title', 'children'), # bottle sold
     Output('card_total_bottle', 'children'),
-    Input('store_y', 'data'),
-)
-def selected_y(y_selected):
-    global title, total_sales
-
-    if y_selected is None:
-        title = f"Year: N/A"
-        total_sales = "$0"
-    else:
-        # 2012
-        if y_selected == 1:
-            title = f"Year: 2012"
-            total_sales = "$100,000,000"
-
-        # 2013
-        if y_selected == 2:
-            title = f"Year: 2013"
-            total_sales = "$100,000,000"
-
-        # 2014
-        if y_selected == 3:
-            title = f"Year: 2014"
-            total_sales = "$100,000,000"
-
-        # 2015
-        if y_selected == 4:
-            title = f"Year: 2015"
-            total_sales = "$100,000,000"
-
-        # 2016
-        if y_selected == 5:
-            title = f"Year: 2016"
-            total_sales = "$100,000,000"
-
-        # 2017
-        if y_selected == 6:
-            title = f"Year: 2017"
-            total_sales = "$100,000,000"
-
-        # 2018
-        if y_selected == 7:
-            title = f"Year: 2018"
-            total_sales = "$100,000,000"
-
-        # 2019
-        if y_selected == 8:
-            title = f"Year: 2019"
-            total_sales = "$100,000,000"
-
-        # 2020
-        if y_selected == 9:
-            title = f"Year: 2020"
-            total_sales = "$100,000,000"
-
-        # 2021
-        if y_selected == 10:
-            title = f"Year: 2021"
-            total_sales = "$100,000,000"
-
-        # 2022
-        if y_selected == 11:
-            title = f"Year: 2022"
-            total_sales = "$100,000,000"
-
-    return title, total_sales
-
-# -------------------------------------------------
-@callback(
-    Output('card_volume_title', 'children'),
+    Output('card_volume_title', 'children'), # volume sold
     Output('card_total_volume', 'children'),
-    Input('store_y', 'data'),
-)
-def selected_y(y_selected):
-    global title, total_sales
-
-    if y_selected is None:
-        title = f"Year: N/A"
-        total_sales = "$0"
-    else:
-        # 2012
-        if y_selected == 1:
-            title = f"Year: 2012"
-            total_sales = "$100,000,000"
-
-        # 2013
-        if y_selected == 2:
-            title = f"Year: 2013"
-            total_sales = "$100,000,000"
-
-        # 2014
-        if y_selected == 3:
-            title = f"Year: 2014"
-            total_sales = "$100,000,000"
-
-        # 2015
-        if y_selected == 4:
-            title = f"Year: 2015"
-            total_sales = "$100,000,000"
-
-        # 2016
-        if y_selected == 5:
-            title = f"Year: 2016"
-            total_sales = "$100,000,000"
-
-        # 2017
-        if y_selected == 6:
-            title = f"Year: 2017"
-            total_sales = "$100,000,000"
-
-        # 2018
-        if y_selected == 7:
-            title = f"Year: 2018"
-            total_sales = "$100,000,000"
-
-        # 2019
-        if y_selected == 8:
-            title = f"Year: 2019"
-            total_sales = "$100,000,000"
-
-        # 2020
-        if y_selected == 9:
-            title = f"Year: 2020"
-            total_sales = "$100,000,000"
-
-        # 2021
-        if y_selected == 10:
-            title = f"Year: 2021"
-            total_sales = "$100,000,000"
-
-        # 2022
-        if y_selected == 11:
-            title = f"Year: 2022"
-            total_sales = "$100,000,000"
-
-    return title, total_sales
-
-# -------------------------------------------------
-@callback(
-    Output('card_cost_title', 'children'),
+    Output('card_cost_title', 'children'), # cost
     Output('card_total_cost', 'children'),
-    Input('store_y', 'data'),
-)
-def selected_y(y_selected):
-    global title, total_sales
-
-    if y_selected is None:
-        title = f"Year: N/A"
-        total_sales = "$0"
-    else:
-        # 2012
-        if y_selected == 1:
-            title = f"Year: 2012"
-            total_sales = "$100,000,000"
-
-        # 2013
-        if y_selected == 2:
-            title = f"Year: 2013"
-            total_sales = "$100,000,000"
-
-        # 2014
-        if y_selected == 3:
-            title = f"Year: 2014"
-            total_sales = "$100,000,000"
-
-        # 2015
-        if y_selected == 4:
-            title = f"Year: 2015"
-            total_sales = "$100,000,000"
-
-        # 2016
-        if y_selected == 5:
-            title = f"Year: 2016"
-            total_sales = "$100,000,000"
-
-        # 2017
-        if y_selected == 6:
-            title = f"Year: 2017"
-            total_sales = "$100,000,000"
-
-        # 2018
-        if y_selected == 7:
-            title = f"Year: 2018"
-            total_sales = "$100,000,000"
-
-        # 2019
-        if y_selected == 8:
-            title = f"Year: 2019"
-            total_sales = "$100,000,000"
-
-        # 2020
-        if y_selected == 9:
-            title = f"Year: 2020"
-            total_sales = "$100,000,000"
-
-        # 2021
-        if y_selected == 10:
-            title = f"Year: 2021"
-            total_sales = "$100,000,000"
-
-        # 2022
-        if y_selected == 11:
-            title = f"Year: 2022"
-            total_sales = "$100,000,000"
-
-    return title, total_sales
-
-# -------------------------------------------------
-@callback(
-    Output('card_retail_title', 'children'),
+    Output('card_retail_title', 'children'), # retail
     Output('card_total_retail', 'children'),
     Input('store_y', 'data'),
 )
 def selected_y(y_selected):
-    global title, total_sales
+    global sale_title, total_sales, inv_title, total_inv, bottle_title, total_bottle, volume_title, \
+        total_volume, cost_title, total_cost, retail_title, total_retail
 
     if y_selected is None:
-        title = f"Year: N/A"
+        sale_title = f"Year: N/A"
         total_sales = "$0"
+        inv_title = f"Year: N/A"
+        total_inv = "$0"
+        bottle_title = f"Year: N/A"
+        total_bottle = "$0"
+        volume_title = f"Year: N/A"
+        total_volume = "$0"
+        cost_title = f"Year: N/A"
+        total_cost = "$0"
+        retail_title = f"Year: N/A"
+        total_retail = "$0"
     else:
         # 2012
         if y_selected == 1:
-            title = f"Year: 2012"
+            sale_title = f"Year: 2012"
             total_sales = "$100,000,000"
+            inv_title = f"Year: 2012"
+            total_inv = "$100,000,000"
+            bottle_title = f"Year: 2012"
+            total_bottle = "$100,000,000"
+            volume_title = f"Year: 2012"
+            total_volume = "$100,000,000"
+            cost_title = f"Year: 2012"
+            total_cost = "$100,000,000"
+            retail_title = f"Year: 2012"
+            total_retail = "$100,000,000"
 
         # 2013
         if y_selected == 2:
-            title = f"Year: 2013"
+            sale_title = f"Year: 2013"
             total_sales = "$100,000,000"
+            inv_title = f"Year: 2013"
+            total_inv = "$100,000,000"
+            bottle_title = f"Year: 2013"
+            total_bottle = "$100,000,000"
+            volume_title = f"Year: 2013"
+            total_volume = "$100,000,000"
+            cost_title = f"Year: 2013"
+            total_cost = "$100,000,000"
+            retail_title = f"Year: 2013"
+            total_retail = "$100,000,000"
 
         # 2014
         if y_selected == 3:
-            title = f"Year: 2014"
+            sale_title = f"Year: 2014"
             total_sales = "$100,000,000"
+            inv_title = f"Year: 2014"
+            total_inv = "$100,000,000"
+            bottle_title = f"Year: 2014"
+            total_bottle = "$100,000,000"
+            volume_title = f"Year: 2014"
+            total_volume = "$100,000,000"
+            cost_title = f"Year: 2014"
+            total_cost = "$100,000,000"
+            retail_title = f"Year: 2014"
+            total_retail = "$100,000,000"
 
         # 2015
         if y_selected == 4:
-            title = f"Year: 2015"
+            sale_title = f"Year: 2015"
             total_sales = "$100,000,000"
+            inv_title = f"Year: 2015"
+            total_inv = "$100,000,000"
+            bottle_title = f"Year: 2015"
+            total_bottle = "$100,000,000"
+            volume_title = f"Year: 2015"
+            total_volume = "$100,000,000"
+            cost_title = f"Year: 2015"
+            total_cost = "$100,000,000"
+            retail_title = f"Year: 2015"
+            total_retail = "$100,000,000"
 
         # 2016
         if y_selected == 5:
-            title = f"Year: 2016"
+            sale_title = f"Year: 2016"
             total_sales = "$100,000,000"
+            inv_title = f"Year: 2016"
+            total_inv = "$100,000,000"
+            bottle_title = f"Year: 2016"
+            total_bottle = "$100,000,000"
+            volume_title = f"Year: 2016"
+            total_volume = "$100,000,000"
+            cost_title = f"Year: 2016"
+            total_cost = "$100,000,000"
+            retail_title = f"Year: 2016"
+            total_retail = "$100,000,000"
 
         # 2017
         if y_selected == 6:
-            title = f"Year: 2017"
+            sale_title = f"Year: 2017"
             total_sales = "$100,000,000"
+            inv_title = f"Year: 2017"
+            total_inv = "$100,000,000"
+            bottle_title = f"Year: 2017"
+            total_bottle = "$100,000,000"
+            volume_title = f"Year: 2017"
+            total_volume = "$100,000,000"
+            cost_title = f"Year: 2017"
+            total_cost = "$100,000,000"
+            retail_title = f"Year: 2017"
+            total_retail = "$100,000,000"
 
         # 2018
         if y_selected == 7:
-            title = f"Year: 2018"
+            sale_title = f"Year: 2018"
             total_sales = "$100,000,000"
+            inv_title = f"Year: 2018"
+            total_inv = "$100,000,000"
+            bottle_title = f"Year: 2018"
+            total_bottle = "$100,000,000"
+            volume_title = f"Year: 2018"
+            total_volume = "$100,000,000"
+            cost_title = f"Year: 2018"
+            total_cost = "$100,000,000"
+            retail_title = f"Year: 2018"
+            total_retail = "$100,000,000"
 
         # 2019
         if y_selected == 8:
-            title = f"Year: 2019"
+            sale_title = f"Year: 2019"
             total_sales = "$100,000,000"
+            inv_title = f"Year: 2019"
+            total_inv = "$100,000,000"
+            bottle_title = f"Year: 2019"
+            total_bottle = "$100,000,000"
+            volume_title = f"Year: 2019"
+            total_volume = "$100,000,000"
+            cost_title = f"Year: 2019"
+            total_cost = "$100,000,000"
+            retail_title = f"Year: 2019"
+            total_retail = "$100,000,000"
 
         # 2020
         if y_selected == 9:
-            title = f"Year: 2020"
+            sale_title = f"Year: 2020"
             total_sales = "$100,000,000"
+            inv_title = f"Year: 2020"
+            total_inv = "$100,000,000"
+            bottle_title = f"Year: 2020"
+            total_bottle = "$100,000,000"
+            volume_title = f"Year: 2020"
+            total_volume = "$100,000,000"
+            cost_title = f"Year: 2020"
+            total_cost = "$100,000,000"
+            retail_title = f"Year: 2020"
+            total_retail = "$100,000,000"
 
         # 2021
         if y_selected == 10:
-            title = f"Year: 2021"
+            sale_title = f"Year: 2021"
             total_sales = "$100,000,000"
+            inv_title = f"Year: 2021"
+            total_inv = "$100,000,000"
+            bottle_title = f"Year: 2021"
+            total_bottle = "$100,000,000"
+            volume_title = f"Year: 2021"
+            total_volume = "$100,000,000"
+            cost_title = f"Year: 2021"
+            total_cost = "$100,000,000"
+            retail_title = f"Year: 2021"
+            total_retail = "$100,000,000"
 
         # 2022
         if y_selected == 11:
-            title = f"Year: 2022"
+            sale_title = f"Year: 2022"
             total_sales = "$100,000,000"
+            inv_title = f"Year: 2022"
+            total_inv = "$100,000,000"
+            bottle_title = f"Year: 2022"
+            total_bottle = "$100,000,000"
+            volume_title = f"Year: 2022"
+            total_volume = "$100,000,000"
+            cost_title = f"Year: 2022"
+            total_cost = "$100,000,000"
+            retail_title = f"Year: 2022"
+            total_retail = "$100,000,000"
 
-    return title, total_sales
+    return sale_title, total_sales, inv_title, total_inv, bottle_title, total_bottle, volume_title, \
+        total_volume, cost_title, total_cost, retail_title, total_retail
