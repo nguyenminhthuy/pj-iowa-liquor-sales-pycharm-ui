56b669db63fde9115d04f3214ad5cb300f07eea3
Nguyen Minh Thuy <ntmthuy.1264140@gmail.com>
2023-09-25 15:23:53 +0700

    Folium maps for store location

--- pages/details.py
+++ pages/details.py
@@ -37,163 +37,136 @@ layout = html.Div([
         ], className="m-4",
     ),
 
-    dcc.Tabs([
-        dcc.Tab(label='Tab one', children=[
-            dbc.Row([
-                dbc.Col([
-                    dcc.Graph(
-                        figure={
-                            'data': [
-                                {'x': [1, 2, 3], 'y': [4, 1, 2],
-                                    'type': 'bar', 'name': 'SF'},
-                                {'x': [1, 2, 3], 'y': [2, 4, 5],
-                                 'type': 'bar', 'name': 'Montréal'},
-                            ]
-                        }
-                    )
-                ]),
-                dbc.Col([
-                    dcc.Graph(
-                        figure={
-                            'data': [
-                                {'x': [1, 2, 3], 'y': [4, 1, 2],
-                                    'type': 'bar', 'name': 'SF'},
-                                {'x': [1, 2, 3], 'y': [2, 4, 5],
-                                 'type': 'bar', 'name': 'Montréal'},
-                            ]
-                        }
-                    )
+    html.Div(
+        dbc.Row([
+                dbc.Col(html.H3("SALES ANALYSIS", style={"color": "#FF7F50"})),
+            ], className="m-5"
+        )
+    ),
+
+    html.Div(
+        dbc.Row([
+            dbc.Col([
+                dcc.Tabs([
+                    dcc.Tab(label='TOP 10 ITEMS', children=[
+                        dbc.Row([
+                            dbc.Col(cards.top_highestItem_graph, className="m-4"),
+                            dbc.Col(cards.top_mostItem_graph, className="m-3"),
+                        ])
+                    ], style=tab_style, selected_style=tab_selected_style),
+
+                    dcc.Tab(label='TOP 10 CATEGORIES', children=[
+                        dbc.Row([
+                            dbc.Col(cards.top_highestCat_graph, className="m-4"),
+                            dbc.Col(cards.top_mostCat_graph, className="m-4"),
+                        ])
+                    ], style=tab_style, selected_style=tab_selected_style),
+
+                    dcc.Tab(label='TOP 10 VENDORS', children=[
+                        dbc.Row([
+                            dbc.Col(cards.top_highestVendor_graph, className="m-4"),
+                            dbc.Col(cards.top_mostVendor_graph, className="m-4"),
+                        ])
+                    ], style=tab_style, selected_style=tab_selected_style),
+
+                    dcc.Tab(label='TOP 10 STORES', children=[
+                        dbc.Row([
+                            dbc.Col(cards.top_highestStore_graph, className="m-4"),
+                            dbc.Col(cards.top_mostStore_graph, className="m-4"),
+                        ])
+                    ], style=tab_style, selected_style=tab_selected_style),
                 ])
             ])
-        ], style=tab_style, selected_style=tab_selected_style),
-        dcc.Tab(label='Tab two', children=[
-            dcc.Graph(
-                figure={
-                    'data': [
-                        {'x': [1, 2, 3], 'y': [1, 4, 1],
-                            'type': 'bar', 'name': 'SF'},
-                        {'x': [1, 2, 3], 'y': [1, 2, 3],
-                         'type': 'bar', 'name': 'Montréal'},
-                    ]
-                }
-            )
-        ], style=tab_style, selected_style=tab_selected_style),
-        dcc.Tab(label='Tab three', children=[
-            dcc.Graph(
-                figure={
-                    'data': [
-                        {'x': [1, 2, 3], 'y': [2, 4, 3],
-                            'type': 'bar', 'name': 'SF'},
-                        {'x': [1, 2, 3], 'y': [5, 4, 3],
-                         'type': 'bar', 'name': 'Montréal'},
-                    ]
-                }
-            )
-        ], style=tab_style, selected_style=tab_selected_style),
-    ], className="m-4"),
-
-    dbc.Row(
-        [
-            html.H3("SALES ANALYSIS BY ITEM", style={"color": "#FF7F50"}),
-            dbc.Col(cards.top_highestItem_graph, className="text-center border-end m-3"),
-            dbc.Col(cards.top_mostItem_graph, className="text-center border-end m-3"),
-        ], className="m-4",
+        ], className="m-5"
+        )
     ),
 
     dbc.Row(
         [
-            html.H3("SALES ANALYSIS BY CATEGORY", style={"color": "#FF7F50"}),
-            dbc.Col(cards.top_highestCat_graph, className="text-center border-end m-3"),
-            dbc.Col(cards.top_mostCat_graph, className="text-center border-end m-3"),
-        ], className="m-4",
-    ),
-
-    dbc.Row(
-        [
-            html.H3("SALES ANALYSIS BY VENDOR", style={"color": "#FF7F50"}),
-            dbc.Col(cards.top_highestVendor_graph, className="text-center border-end m-3"),
-            dbc.Col(cards.top_mostVendor_graph, className="text-center border-end m-3"),
-        ], className="m-4",
-    ),
-
-    dbc.Row(
-        [
-            html.H3("SALES ANALYSIS BY STORE", style={"color": "#FF7F50"}),
-            dbc.Col(cards.top_highestStore_graph, className="text-center border-end m-3"),
-            dbc.Col(cards.top_mostStore_graph, className="text-center border-end m-3"),
-        ], className="m-4",
-    ),
-
-    dbc.Row(
-        [
-            html.H3("SALES ANALYSIS BY CITY", style={"color": "#FF7F50"}),
-            dbc.Col(cards.top_highestCity_graph, className="text-center border-end m-3"),
-            dbc.Col(cards.top_mostCity_graph, className="text-center border-end m-3"),
-        ], className="m-4",
-    ),
-
-    dbc.Row(
-        [
-            html.H3("SALES ANALYSIS BY COUNTY", style={"color": "#FF7F50"}),
-            dbc.Col(cards.top_highestCounty_graph, className="text-center border-end m-3"),
-            dbc.Col(cards.top_mostCounty_graph, className="text-center border-end m-3"),
-        ], className="m-4",
+            html.H3("STORE LOCATION", style={"color": "#FF7F50"}),
+            html.Div(id='folium-map-container')
+        ], className="m-5",
     ),
 ])
 
 
 @callback(
     Output('overview_title', 'children'),
+    Output('folium-map-container', 'children'),
     Input('store_y', 'data'),
 )
 def selected_y(y_selected):
-    global overview_title
+    global overview_title, folium_map
 
     if y_selected is None:
         overview_title = f"SALES ANALYSIS OVERVIEW (N/A)"
+        folium_map = html.Iframe(srcDoc = open('img/map_2012.html', 'r').read(),
+                                 width='100%', height=500)
     else:
         # 2012
         if y_selected == 1:
             overview_title = f"SALES ANALYSIS OVERVIEW (2012)"
+            folium_map = html.Iframe(srcDoc=open('img/map_2012.html', 'r').read(),
+                                     width='100%', height=500)
 
         # 2013
         if y_selected == 2:
             overview_title = f"SALES ANALYSIS OVERVIEW (2013)"
+            folium_map = html.Iframe(srcDoc=open('img/map_2012.html', 'r').read(),
+                                     width='100%', height=500)
 
         # 2014
         if y_selected == 3:
             overview_title = f"SALES ANALYSIS OVERVIEW (2014)"
+            folium_map = html.Iframe(srcDoc=open('img/map_2012.html', 'r').read(),
+                                     width='100%', height=500)
 
         # 2015
         if y_selected == 4:
             overview_title = f"SALES ANALYSIS OVERVIEW (2015)"
+            folium_map = html.Iframe(srcDoc=open('img/map_2012.html', 'r').read(),
+                                     width='100%', height=500)
 
         # 2016
         if y_selected == 5:
             overview_title = f"SALES ANALYSIS OVERVIEW (2016)"
+            folium_map = html.Iframe(srcDoc=open('img/map_2012.html', 'r').read(),
+                                     width='100%', height=500)
 
         # 2017
         if y_selected == 6:
             overview_title = f"SALES ANALYSIS OVERVIEW (2017)"
+            folium_map = html.Iframe(srcDoc=open('img/map_2012.html', 'r').read(),
+                                     width='100%', height=500)
 
         # 2018
         if y_selected == 7:
             overview_title = f"SALES ANALYSIS OVERVIEW (2018)"
+            folium_map = html.Iframe(srcDoc=open('img/map_2012.html', 'r').read(),
+                                     width='100%', height=500)
 
         # 2019
         if y_selected == 8:
             overview_title = f"SALES ANALYSIS OVERVIEW (2019)"
+            folium_map = html.Iframe(srcDoc=open('img/map_2012.html', 'r').read(),
+                                     width='100%', height=500)
 
         # 2020
         if y_selected == 9:
             overview_title = f"SALES ANALYSIS OVERVIEW (2020)"
+            folium_map = html.Iframe(srcDoc=open('img/map_2012.html', 'r').read(),
+                                     width='100%', height=500)
 
         # 2021
         if y_selected == 10:
             overview_title = f"SALES ANALYSIS OVERVIEW (2021)"
+            folium_map = html.Iframe(srcDoc=open('img/map_2012.html', 'r').read(),
+                                     width='100%', height=500)
 
         # 2022
         if y_selected == 11:
             overview_title = f"SALES ANALYSIS OVERVIEW (2022)"
+            folium_map = html.Iframe(srcDoc=open('img/map_2012.html', 'r').read(),
+                                     width='100%', height=500)
 
-    return overview_title
+    return overview_title, folium_map
